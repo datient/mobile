cbe6be5f20ad88245fef27627ccbeaa1cde3fc53
Ivan Nuñez <ivan.nunez@25watts.com.ar>
2019-06-08 19:38:28 -0300

    Add form validation

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -2,7 +2,7 @@ import 'package:datient/models/doctor.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
-  LoginPage({Key key, this.title,}) : super(key: key);
+  LoginPage({Key key, this.title}) : super(key: key);
 
   final String title;
 
@@ -11,95 +11,113 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  _showSnackBar() {
-    final snackBar = SnackBar(
-      content: Text('El Usuario/Correo ingresado son incorrectos'),
-      backgroundColor: Colors.red,
+  final GlobalKey<FormState> _formKey = new GlobalKey<FormState>();
+  final _mailController = TextEditingController();
+  final _pwController = TextEditingController();
+
+  Widget loginForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 60),
+          TextFormField(
+            controller: _mailController,
+            validator: (value) {
+              if (value.isEmpty || !value.contains('@')) {
+                return 'Por favor, ingrese su correo electronico';
+              }
+            },
+            decoration: InputDecoration(
+              labelText: 'Correo Electronico',
+              border: OutlineInputBorder(
+                borderRadius: const BorderRadius.all(
+                  const Radius.circular(10.0),
+                ),
+              ),
+              filled: true,
+              hintStyle: TextStyle(color: Colors.grey[800]),
+              hintText: "Ingrese su correo",
+              fillColor: Colors.white70,
+            ),
+            keyboardType: TextInputType.emailAddress,
+          ),
+          SizedBox(height: 18),
+          TextFormField(
+            controller: _pwController,
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Por favor, ingrese su correo contraseña';
+              }
+            },
+            decoration: InputDecoration(
+                labelText: 'Contraseña',
+                border: OutlineInputBorder(
+                  borderRadius: const BorderRadius.all(
+                    const Radius.circular(10.0),
+                  ),
+                ),
+                filled: true,
+                hintStyle: TextStyle(color: Colors.grey[800]),
+                hintText: "Ingrese su contraseña",
+                fillColor: Colors.white70),
+            obscureText: true,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: RaisedButton(
+                  onPressed: () {
+                    _validateForm();
+                  },
+                  child: Text('Iniciar Sesion'),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(15),
+                child: RaisedButton(
+                  onPressed: () {},
+                  child: Text('Registrarse'),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
-    _scaffoldKey.currentState.showSnackBar(snackBar);
+  }
+
+  _validateForm() {
+    if (_formKey.currentState.validate()) {
+      String mail = _mailController.value.text;
+      String password = _pwController.value.text;
+      var doctor = Doctor();
+      Future<bool> token = doctor.obtainToken(mail, password);
+      token.then((success) {
+        if (success == true) {
+          Navigator.of(context).pushReplacementNamed('/home');
+        }
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    final mailcontroller = TextEditingController();
-    final pwcontroller = TextEditingController();
     return Scaffold(
-      key: _scaffoldKey,
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: TextFormField(
-                decoration: new InputDecoration(
-                    labelText: 'Correo Electronico',
-                    border: new OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(10.0),
-                      ),
-                    ),
-                    filled: true,
-                    hintStyle: new TextStyle(color: Colors.grey[800]),
-                    hintText: "Ingrese su correo",
-                    fillColor: Colors.white70),
-                controller: mailcontroller,
-                keyboardType: TextInputType.emailAddress,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: TextFormField(
-                decoration: new InputDecoration(
-                    labelText: 'Contraseña',
-                    border: new OutlineInputBorder(
-                      borderRadius: const BorderRadius.all(
-                        const Radius.circular(10.0),
-                      ),
-                    ),
-                    filled: true,
-                    hintStyle: new TextStyle(color: Colors.grey[800]),
-                    hintText: "Ingrese su contraseña",
-                    fillColor: Colors.white70),
-                obscureText: true,
-                controller: pwcontroller,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: RaisedButton(
-                      onPressed: () {
-                        var doctor = Doctor();
-                        doctor.obtainToken(
-                          mailcontroller.text,
-                          pwcontroller.text,
-                        );
-                        Navigator.of(context).pushReplacementNamed('/home');
-                      },
-                      child: Text('Iniciar Sesion'),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(15),
-                    child: RaisedButton(
-                      onPressed: () {},
-                      child: Text('Registrarse'),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      body: SafeArea(
+        child: Builder(builder: (BuildContext context) {
+          return ListView(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            children: <Widget>[
+              loginForm(),
+            ],
+          );
+        }),
       ),
     );
   }
