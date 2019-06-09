c256186414049676d38ff8f5345b404690f90d11
Ivan Nuñez <ivanluis.cba@gmail.com>
2019-06-09 16:17:11 -0300

    Fix future gridview list

--- lib/models/room.dart
+++ lib/models/room.dart
@@ -1,20 +1,16 @@
-import 'package:http/http.dart' as http;
-import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:convert' as JSON;
-
 class Room {
   var id;
   var roomName;
 
-  Future getRoom() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    var _token = prefs.getString('token');
-    print(_token);
-    final response = await http.get(
-      'http://10.0.2.2:8000/api/room/',
-      headers: {'Authorization': 'JWT $_token'},
+  Room({
+    this.id,
+    this.roomName,
+  });
+
+  factory Room.fromJson(Map<String, dynamic> json) {
+    return Room(
+      id: json['id'],
+      roomName: json['name'],
     );
-    var extractdata = JSON.jsonDecode(response.body);
-    return extractdata;
   }
 }
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,6 +1,9 @@
+import 'dart:convert' as JSON;
 import 'package:datient/models/doctor.dart';
 import 'package:datient/models/room.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -10,8 +13,22 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var token;
   Doctor doctor = Doctor();
-  Room room = Room();
-  
+
+  Future<List<Room>> _getRooms() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var _token = prefs.getString('token');
+    List list;
+    final response = await http.get(
+      'http://10.0.2.2:8000/api/room/',
+      headers: {'Authorization': 'JWT $_token'},
+    );
+    if (response.statusCode == 200) {
+      final extractdata = JSON.jsonDecode(response.body) as List;
+      list = extractdata.map((json) => Room.fromJson(json)).toList();
+    }
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,37 +58,41 @@ class _HomePageState extends State<HomePage> {
             Expanded(
               flex: 9,
               child: FutureBuilder(
-                future: this.room.getRoom(),
+                future: _getRooms(),
                 builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  return GridView.count(
-                    crossAxisCount: 2,
-                    children: List.generate(snapshot.data.length, (i) {
-                      return Container(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.of(context).pushNamed('/room');
-                          },
-                          child: Card(
-                            child: Center(
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(
-                                    Icons.local_hospital,
-                                    size: 80,
+                  return snapshot.data != null
+                      ? GridView.count(
+                          crossAxisCount: 2,
+                          children:
+                              List.generate(snapshot.data.length, (index) {
+                            return Container(
+                              child: GestureDetector(
+                                onTap: () {
+                                  Navigator.of(context).pushNamed('/room');
+                                },
+                                child: Card(
+                                  child: Center(
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Icon(
+                                          Icons.local_hospital,
+                                          size: 80,
+                                        ),
+                                        Text(
+                                          snapshot.data[index].roomName,
+                                          style: TextStyle(fontSize: 20),
+                                        ),
+                                      ],
+                                    ),
                                   ),
-                                  Text(
-                                    snapshot.data[i]['name'],
-                                    style: TextStyle(fontSize: 20),
-                                  ),
-                                ],
+                                ),
                               ),
-                            ),
-                          ),
-                        ),
-                      );
-                    }),
-                  );
+                            );
+                          }),
+                        )
+                      : Center(child: CircularProgressIndicator());
                 },
               ),
             ),
