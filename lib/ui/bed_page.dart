406ca815b2884ec0d0928f1a620a5edc6cd8ee33
Facundo <facu1910@gmail.com>
2019-07-23 11:25:43 -0300

    Create discharge and assign patient floating action button

--- lib/ui/bed_page.dart
+++ lib/ui/bed_page.dart
@@ -19,57 +19,67 @@ class _BedPageState extends State<BedPage> {
   Widget build(BuildContext context) {
     var _hospitalizations = widget.bed.hospitalizations;
     return Scaffold(
-        appBar: AppBar(
-          title: Text('${widget.bed.bedName}'),
-        ),
-        body: _hospitalizations.isNotEmpty
-            ? ListView.builder(
-                itemCount: 1,
-                itemBuilder: (BuildContext context, int index) {
-                  Hospitalization hospitalizations = _hospitalizations.last;
-                  return Container(
-                    child: Card(
-                      margin: EdgeInsets.all(15),
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(15)),
-                      elevation: 6,
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text('Hospitalizacion',
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.bold)),
-                            Divider(),
-                            Text('Paciente internado',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.grey)),
-                            Text(
-                              hospitalizations.hospitalizedPatient.toString(),
-                              style: TextStyle(fontSize: 18),
-                            ),
-                            Divider(),
-                            Text('Fecha de ingreso',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.grey)),
-                            Text(hospitalizations.entryDate,
-                                style: TextStyle(fontSize: 18)),
-                            Divider(),
-                            Text('Atendido por',
-                                style: TextStyle(
-                                    fontSize: 16, color: Colors.grey)),
-                            Text(hospitalizations.doctorInCharge.toString(),
-                                style: TextStyle(fontSize: 18)),
-                            Divider(),
-                          ],
-                        ),
+      appBar: AppBar(
+        title: Text('${widget.bed.bedName}'),
+      ),
+      body: _hospitalizations.isNotEmpty
+          ? ListView.builder(
+              itemCount: 1,
+              itemBuilder: (BuildContext context, int index) {
+                Hospitalization hospitalizations = _hospitalizations.last;
+                return Container(
+                  child: Card(
+                    margin: EdgeInsets.all(15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15)),
+                    elevation: 6,
+                    child: Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Hospitalizacion',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold)),
+                          Divider(),
+                          Text('Paciente internado',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey)),
+                          Text(
+                            hospitalizations.hospitalizedPatient.toString(),
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Divider(),
+                          Text('Fecha de ingreso',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey)),
+                          Text(hospitalizations.entryDate,
+                              style: TextStyle(fontSize: 18)),
+                          Divider(),
+                          Text('Atendido por',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.grey)),
+                          Text(hospitalizations.doctorInCharge.toString(),
+                              style: TextStyle(fontSize: 18)),
+                          Divider(),
+                        ],
                       ),
                     ),
-                  );
-                })
-            : Center(
-                child: Text('No se han encontrado hospitalizaciones'),
-              ));
+                  ),
+                );
+              })
+          : Center(
+              child: Text('No se han encontrado hospitalizaciones'),
+            ),
+      floatingActionButton: _hospitalizations.isNotEmpty
+          ? FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.assignment_turned_in),
+            )
+          : FloatingActionButton(
+              onPressed: () {},
+              child: Icon(Icons.add),
+            ),
+    );
   }
 }
