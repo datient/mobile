5eb053317fabbd2f213a54030d50d9b492f5cd7d
Facundo <facu1910@gmail.com>
2019-10-09 11:29:44 -0300

    Add piechart

--- lib/ui/statistics_page.dart
+++ lib/ui/statistics_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'indicator.dart';
 
 class StatisticsPage extends StatefulWidget {
   StatisticsPage({
@@ -10,13 +13,162 @@ class StatisticsPage extends StatefulWidget {
 }
 
 class _StatisticsPageState extends State<StatisticsPage> {
+  StreamController<PieTouchResponse> pieTouchedResultStreamController;
+  int touchedIndex;
+
   @override
+  void initState() {
+    super.initState();
+
+    pieTouchedResultStreamController = StreamController();
+    pieTouchedResultStreamController.stream.distinct().listen((details) {
+      if (details == null) {
+        return;
+      }
+
+      setState(() {
+        if (details.touchInput is FlLongPressEnd) {
+          touchedIndex = -1;
+        } else {
+          touchedIndex = details.touchedSectionPosition;
+        }
+      });
+    });
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Estadísticas'),
+      appBar: AppBar(title: Text('Estadisticas'),),
+          body: AspectRatio(
+        aspectRatio: 0.69,
+        child: Card(
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              const SizedBox(
+                height: 18,
+              ),
+              Expanded(
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: FlChart(
+                    chart: PieChart(
+                      PieChartData(
+                          pieTouchData: PieTouchData(
+                              touchResponseStreamSink:
+                                  pieTouchedResultStreamController.sink),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          sectionsSpace: 0,
+                          centerSpaceRadius: 40,
+                          sections: showingSections()),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const <Widget>[
+                  Indicator(
+                    color: Color(0xff0293ee),
+                    text: 'First',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xfff8b250),
+                    text: 'Second',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xff845bef),
+                    text: 'Third',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 4,
+                  ),
+                  Indicator(
+                    color: Color(0xff13d38e),
+                    text: 'Fourth',
+                    isSquare: true,
+                  ),
+                  SizedBox(
+                    height: 18,
+                  ),
+                ],
+              ),
+              const SizedBox(
+                width: 28,
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Container(),
     );
   }
-}
+
+  List<PieChartSectionData> showingSections() {
+    return List.generate(4, (i) {
+      final isTouched = i == touchedIndex;
+      final double fontSize = isTouched ? 25 : 16;
+      final double radius = isTouched ? 60 : 50;
+      switch (i) {
+        case 0:
+          return PieChartSectionData(
+            color: const Color(0xff0293ee),
+            value: 40,
+            title: '40%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 1:
+          return PieChartSectionData(
+            color: const Color(0xfff8b250),
+            value: 30,
+            title: '30%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 2:
+          return PieChartSectionData(
+            color: const Color(0xff845bef),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        case 3:
+          return PieChartSectionData(
+            color: const Color(0xff13d38e),
+            value: 15,
+            title: '15%',
+            radius: radius,
+            titleStyle: TextStyle(
+                fontSize: fontSize,
+                fontWeight: FontWeight.bold,
+                color: const Color(0xffffffff)),
+          );
+        default:
+          return null;
+      }
+    });
+  }
+  }
